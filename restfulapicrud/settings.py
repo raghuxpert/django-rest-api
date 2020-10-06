1f9d27bb2bc383e08bfb6d50fad464b93e130ef1
raghuxpert <62813845+raghuxpert@users.noreply.github.com>
2020-10-06 16:04:25 -0400

    a

--- restfulapicrud/settings.py
+++ restfulapicrud/settings.py
@@ -83,7 +83,7 @@ DATABASES = {
         'NAME': 'raghav',
         'USER': 'postgres',
         'PASSWORD': '1234',
-        'HOST': '127.0.0.1',
+        'HOST': '192.168.0.15',
         'PORT': '5432'
     }
 }
